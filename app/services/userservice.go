a865da5dfdd515a1839c1457d80dbc3a99932aef
hshzspndd <2548358316@qq.com>
2026-09-11 14:15:43 +0800

    完善了注册时密码安全存储

--- app/services/userservice.go
+++ app/services/userservice.go
@@ -2,11 +2,13 @@ package services
 
 import (
 	"L-F/app/models"
+	"L-F/app/utiles"
 	"L-F/configs/database"
 
 	"gorm.io/gorm"
 )
 
+// 注册时检验用户是否存在
 func CheckUserExistWhenRegister(userName string) bool {
 	err := database.DB.Model(&models.User{}).Where("user_name = ?", userName).First(&models.User{}).Error
 	if err == gorm.ErrRecordNotFound {
@@ -15,16 +17,27 @@ func CheckUserExistWhenRegister(userName string) bool {
 	return true
 }
 
+// 用户注册
 func Register(userName string, password string, role string) (string, int, int) {
 	var user models.User
+
+	// 将密码转化为哈希存储
+	hash_password, err := utiles.Hash(password)
+	if err != nil {
+		return "密码加密失败", 0, 500
+	}
+
 	user.UserName = userName
-	user.Password = password
+	user.Password = hash_password
 	user.Role = role
+
 	if !CheckUserExistWhenRegister(userName) {
-		err := database.DB.Model(&models.User{}).Create(&user).Error
+		err = database.DB.Model(&models.User{}).Create(&user).Error
 		if err != nil {
-			return "注册失败", 0, 400
+			return "注册失败", 0, 500
 		}
+
+		// 顺利注册
 		return "", user.UserId, 200
 	}
 	return "用户已存在", 0, 409
---
+++ app/utiles/hashpassowrd.go
@@ -0,0 +1,12 @@
+package utiles
+
+import "golang.org/x/crypto/bcrypt"
+
+// 将密码转为哈希
+func Hash(plain string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plain), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
